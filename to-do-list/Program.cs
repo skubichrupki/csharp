5d8a1ae9861049aded0bff0ed85930d915809c34
skubichrupki <skupinski420@outlook.com>
2024-07-21 13:24:16 +0200

    todolist init

--- to-do-list/Program.cs
+++ to-do-list/Program.cs
@@ -1,11 +1,32 @@
-﻿namespace to_do_list;
+﻿using System;
+using System.Data.SqlClient;
+
+namespace to_do_list;
 
 class Program
 {
+    // connection options to sql server
+    static string connectionString = "Server=localhost\\SQLEXPRESS;Database=projects;Integrated Security=True;";
+
     static void Main(string[] args)
     {
+        Read();
+    }
 
-        
+    static void Read()
+    {
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+            string query = "SELECT * FROM [todo].[status]";
 
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataReader reader = command.ExecuteReader();
+
+            while(reader.Read() == true)
+            {
+                Console.WriteLine($"ID: {reader["status_id"]} Description: {reader["status_desc"]}");
+            }
+        }
     }
 }
