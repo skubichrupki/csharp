12e729a11310187ab0e6481b88fbf0bc11e89c66
skubichrupki <skupinski420@outlook.com>
2024-07-22 18:15:47 +0200

    i am lil overwhelmed

--- to-do-list/Program.cs
+++ to-do-list/Program.cs
@@ -11,10 +11,10 @@ class Program
     static void Main(string[] args)
     {
         bool run = true;
+        Console.WriteLine("welcome to to-do list");
         while (run)
         {
-            Read();
-            Console.WriteLine("(1 - add task, 0 - exit)");
+            Console.WriteLine("(1 - add task, 2 - show tasks, 0 - exit)");
             Console.Write("choose action: ");
             try 
             {
@@ -26,13 +26,15 @@ class Program
                     break;
                     case 1: Create();
                     break;
+                    case 2: Read();
+                    break;
                     default: Console.WriteLine("invalid action");
                     break;
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine($"error: {e}");
+                Console.WriteLine($"error: {e} \n try again");
             }
         }
     }
@@ -52,28 +54,38 @@ class Program
 
             // #4 initialize the command instance with query and connection params
             SqlCommand command = new SqlCommand(queryString, connection);  
-
             // execute SELECT query (ExecuteReader) and assign the SELECT data to reader instance of SqlDataReader
             SqlDataReader reader = command.ExecuteReader();
 
+            // print table header in console table format
+            string[] columnNames = ["task_id", "task_desc", "status_desc"];
+            int columnWidth = 15; // adjust the column width
+            for (int i = 0; i < columnNames.Length; i++)
+            {
+                Console.Write(columnNames[i].PadRight(columnWidth));
+            }
+            Console.WriteLine("");
+            
             // show data of each column assigned to reader
             while(reader.Read() == true)
             {
-                // if null in sql then empty string - value = if ? then : else
+                // if null in sql then empty string, ternary: type value = condition ? then : else
                 string task_id = (reader["task_id"] != DBNull.Value ? reader["task_id"].ToString() : string.Empty);
                 string task_desc = (reader["task_desc"] != DBNull.Value ? reader["task_desc"].ToString() : string.Empty);
                 string status_desc = (reader["status_desc"] != DBNull.Value ? reader["status_desc"].ToString() : string.Empty);
-                string result = $"| {task_id.PadRight(4)} | {task_desc.PadRight(10)} | {status_desc.PadRight(10)} |";
-                // print result in a console table format;
-                Console.WriteLine(result);
-                Console.Write("+");
-                for (int i = 1; i < (result.Length-1); i++) 
+                string[] columns = [task_id, task_desc, status_desc];
+                 // print row lines
+                for (int i = 0; i <= (columnWidth * columns.Length); i++)
                 {
                     Console.Write("-");
                 }
-                Console.Write("+");
                 Console.WriteLine("");
-
+                // print table rows in console table format
+                for (int i = 0; i < columns.Length; i++)
+                {
+                    Console.Write(columns[i].PadRight(columnWidth));
+                }
+                Console.WriteLine("");
             }
             reader.Close();
         }
